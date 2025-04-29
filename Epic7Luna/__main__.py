e2510fa91fd7d4b34e9f7353eecf3262986e1fc2
Omar2535 <omar2535@alumni.ubc.ca>
2025-04-28 22:44:12 -0400

    Add comment

--- Epic7Luna/__main__.py
+++ Epic7Luna/__main__.py
@@ -5,6 +5,7 @@ from Epic7Luna.utils.adb_manager import device_setup
 
 
 def main():
+    """Main function to run the Epic7Luna bot command line tool."""
     parser = argparse.ArgumentParser(description="Epic7Luna Bot Command Line Tool")
     parser.add_argument('--refresh', action='store_true', help='Refresh the secret shop')
     args = parser.parse_args()
